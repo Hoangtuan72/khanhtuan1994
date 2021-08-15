cddf58606453793e1bd7caa5b71a561d304256ce
Huỳnh Vũ Cát <nhoxsand@gmail.com>
2021-08-15 13:50:24 +0700

    Edit layout for code javascript

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-
-$(function () {
-  $('#infor-button').hover(function () {
+$(document).ready(function () { //add A page can't be manipulated safely until the document is "ready." jQuery detects this state of readiness for you.
+  $(function () {
+    $('#infor-button').hover(function () {
       $('.information').css('display', 'block');
       $('.hobbies').css('display', 'none');
       $('.job').css('display', 'none');
       $('.intro').css('display', 'none');
 
-  });
-})
-$(function () {
-  $('#hobbies-button').hover(function () {
+    });
+
+    $('#hobbies-button').hover(function () {
       $('.information').css('display', 'none');
       $('.hobbies').css('display', 'block');
       $('.job').css('display', 'none');
       $('.intro').css('display', 'none');
 
-  });
-})
-$(function () {
-  $('#job-button').hover(function () {
+    });
+
+    $('#job-button').hover(function () {
       $('.information').css('display', 'none');
       $('.hobbies').css('display', 'none');
       $('.job').css('display', 'block');
       $('.intro').css('display', 'none');
 
-  });
-})
-$(function () {
-  $('#image').hover(function () {
+    });
+
+    $('#image').hover(function () {
       $('.information').css('display', 'none');
       $('.hobbies').css('display', 'none');
       $('.job').css('display', 'none');
       $('.intro').css('display', 'block');
 
+    });
   });
-})
+});
